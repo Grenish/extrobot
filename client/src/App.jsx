6ebe0efb9870576de022fa39319f89915d8cf76b
Grenish rai <ariestheracer@gmail.com>
2024-03-25 05:59:12 +0530

    Installed Vite and ReactJs

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-slate-950 w-full h-screen">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
